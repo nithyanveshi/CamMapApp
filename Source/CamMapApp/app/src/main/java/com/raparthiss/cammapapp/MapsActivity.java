5e406adcfcd6bd6686609599a0f38683c294bb27
nithyanveshi <sr8h6@mail.umkc.edu>
2016-02-10 23:46:26 -0600

    Addign the source code and activity screens

---
+++ Source/CamMapApp/app/src/main/java/com/raparthiss/cammapapp/MapsActivity.java
@@ -0,0 +1,98 @@
+package com.raparthiss.cammapapp;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.ByteArrayOutputStream;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    public Bitmap bitImage;
+    public Bitmap resizedBitImage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (getIntent().hasExtra("imageInfo")){
+            bitImage = BitmapFactory.decodeByteArray(
+                    getIntent().getByteArrayExtra("imageInfo"), 0, getIntent().getByteArrayExtra("imageInfo").length);
+        /*    ByteArrayOutputStream bs = new ByteArrayOutputStream();
+            bitImage.compress(Bitmap.CompressFormat.JPEG, 50, bs);
+        */
+            resizedBitImage = Bitmap.createScaledBitmap(bitImage,40,40,false);
+        }
+    /*  Bundle bd = getIntent().getExtras();
+        Bitmap bm = bd.getParcelable("image");
+
+        Intent redirect = getIntent();
+        String address = redirect.getStringExtra("addressInfo");
+        System.out.println(address);
+    */
+        // Setting default location to my Home
+        mMap.addMarker(new MarkerOptions().position(new LatLng(18.431934,79.135282)).title("Karimnagar"));
+        LocationManager locMgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = locMgr.getBestProvider(criteria, true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location myLocation = locMgr.getLastKnownLocation(provider);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        double lat = myLocation.getLatitude();
+        double lng = myLocation.getLongitude();
+
+        LatLng latLng = new LatLng(lat, lng);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).icon(BitmapDescriptorFactory.fromBitmap(resizedBitImage)).anchor(0.5f, 1));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(14));
+
+    }
+}
